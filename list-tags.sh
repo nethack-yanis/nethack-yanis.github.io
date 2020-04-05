186709973d807bcafe5f95e38619daa3efba1d1e
nethack-yanis <aosdict+yani@gmail.com>
2020-04-05 00:48:02 -0400

    list-tags.sh: support writing out a CSV file to _data/
    I tried to get the frequencies of each tag with pure Liquid template
    code, and was not able to because it lacks a filter that can count the
    number of times a string appears in an array. So instead, this
    workaround: this script already generates the frequencies of each tag.
    Add an option that writes them out to a data file.
    
    list.tags.sh --write-csv would be a good pre-commit hook.

--- list-tags.sh
+++ list-tags.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-if [ -z $1 ] || [ $1 != "--histogram" ]; then
+if [ -z $1 ]; then
+  # no args: view mode
   grep --no-filename '^tag' _yanis/*.md | \
     sed -e 's/tags: \[//' -e 's/\]//' -e 's/, /,/g' -e '/^$/ d' | \
     tr ',' '\n' | \
     sort | \
     uniq -c | \
     less
-else
+elif [ $1 == "--write-csv" ]; then
+  datafile="_data/tagfreq.csv"
+  echo "count,tag" > $datafile
+  grep --no-filename '^tag' _yanis/*.md | \
+    sed -e 's/tags: \[//' -e 's/\]//' -e 's/, /,/g' -e '/^$/ d' | \
+    tr ',' '\n' | \
+    sort | \
+    uniq -c | \
+    sed 's/^ *\([0-9]*\) *\(.*\)$/\1,\2/' >> $datafile
+elif [ $1 == "--histogram" ]; then
+  # fun diagnostic
   grep --no-filename '^tag' _yanis/*.md | \
     sed -e 's/tags: \[//' -e 's/\]//' -e 's/, /,/g' -e '/^$/ d' | \
     tr ',' '\n' | \
@@ -17,4 +28,6 @@ else
     sort -n | \
     uniq -c | \
     awk '{ printf("%3s\t%s\n", $2, $1); }'
+else
+  echo "Unrecognized argument $1."
 fi
