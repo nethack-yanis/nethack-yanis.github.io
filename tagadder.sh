a2d11a89a143e923b51af2c8d0d9ee237cd64b89
copperwater <aosdict@gmail.com>
2023-03-28 20:50:47 -0400

    Overhaul and modernize tagadder.sh
    - put usage information in a function rather than a comment so that
      incorrect invocations of the script can print it out
    - document the --empty mode in the usage info
    - remove the old mode for adding tags to ideas with no tags, because the
      workflow now is to add tags before/during writing up the idea, and the
      workflow of adding ideas with no tags and then going to tag them later
      is long gone (or possibly never existed. It -- and the name of this
      script -- might be a holdover from when I was adding tags to ideas
      from the nethackwiki archive which didn't have tags.)
    - when there are no matching files, print a message that there are no
      matching files, rather than printing "1 matching files" and exiting
    - fix indentation to 4 spaces

--- tagadder.sh
+++ tagadder.sh
@@ -1,54 +1,56 @@
 #!/bin/bash
 # Utility script for adding/editing tags on ideas.
-# Usage:
-# ./tagadder.sh                        - add tags to untagged ideas in the range specified below
-# ./tagadder.sh grep-pattern           - edit tags on ideas where the tags match grep-pattern
-# ./tagadder.sh --full grep-pattern    - as above, but where anything in the file matches
-# ./tagadder.sh --showall grep-pattern - instead of opening vim, just print all matching files in less
+
+function usage() {
+    echo Usage:
+    echo $0 [grep-pattern]           - edit tags on ideas where the tags match grep-pattern
+    echo $0 --full [grep-pattern]    - as above, but where anything in the file matches
+    echo $0 --showall [grep-pattern] - instead of opening vim, just print all matching files in less
+    echo $0 --empty                  - locate files with an erroneous empty-string tag
+}
 
 if [ -z "$1" ]; then
-  # mode for adding tags to new as-yet-untagged ideas
-  # it will only ask to edit files without any tags; beware of adding only some
-  # tags to an idea, then closing it and forgetting to come back to it!
-  # range of files must be manually specified in the below range.
-  files=$(for i in {00001..00100}; do echo _yanis/$i.md; done)
-  tagsearch=false
+    usage
+    exit 1
 elif [ "$1" == "--showall" ]; then
     files=$(grep -rl "$2" _yanis)
+    if [[ $files == "" ]]; then
+        echo 'There are no matching files to show.'
+        exit 0
+    fi
     more $files | less
     exit 0
 elif [ "$1" == "--full" ]; then
-  # full text search
-  files=$(grep -rl "$2" _yanis)
-  tagsearch=true
+    # full text search
+    files=$(grep -rl "$2" _yanis)
 elif [ "$1" == "--empty" ]; then
-  # find improperly tagged files with an empty tag
-  files=$(grep -rl "^tags.*, *[][,]" _yanis)
-  tagsearch=true
+    # find improperly tagged files with an empty tag
+    files=$(grep -rl "^tags.*, *[][,]" _yanis)
 else
-  # tag search
-  files=$(grep -rl "^tags.*$1" _yanis)
-  tagsearch=true
+    # tag search
+    files=$(grep -rl "^tags.*$1" _yanis)
+fi
+
+if [[ $files == "" ]]; then
+    echo 'There are no matching files to edit.'
+    exit 0
 fi
 
 echo "`echo "$files" | wc -l` matching files"
 for file in $files; do
-  grep -q 'tags: \[\]' "$file"
-  if [ $? -eq 0 ] || $tagsearch; then
     read -p "Edit next file $file? [Ynq] " -n1 yn
     if [ -z "$yn" ]; then
-      yn=" "
+        yn=" "
     fi
     echo
     if echo "$yn" | grep -q '[Yy ]'; then
-      if $tagsearch; then
-        vim $file '+normal 5G8|'
-      else
-        vim $file '+normal 5G8|' '+startinsert'
-      fi
+        if $tagsearch; then
+            vim $file '+normal 5G8|'
+        else
+            vim $file '+normal 5G8|' '+startinsert'
+        fi
     elif echo "$yn" | grep -q '[Qq]'; then
-      break;
+        break;
     fi
     # n is offered as a placebo :-)
-  fi
 done
