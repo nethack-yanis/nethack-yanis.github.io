9d97bda14ca51d462de4171d7b7c765de40e84f2
copperwater <aosdict@gmail.com>
2023-04-02 20:32:03 -0400

    Rewrite list-tags.sh to take --categories argument to list categories

--- list-tags.sh
+++ list-tags.sh
@@ -1,33 +1,60 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-  # no args: view mode
-  grep --no-filename '^tag' _yanis/*.md | \
-    sed -e 's/tags: \[//' -e 's/\]//' -e 's/, /,/g' -e '/^$/ d' | \
-    tr ',' '\n' | \
-    sort | \
-    uniq -c | \
-    less
-elif [ $1 == "--write-csv" ]; then
-  datafile="_data/tagfreq.csv"
-  echo "count,tag" > $datafile
-  grep --no-filename '^tag' _yanis/*.md | \
-    sed -e 's/tags: \[//' -e 's/\]//' -e 's/, /,/g' -e '/^$/ d' | \
-    tr ',' '\n' | \
-    sort | \
-    uniq -c | \
-    sed 's/^ *\([0-9]*\) *\(.*\)$/\1,\2/' >> $datafile
-elif [ $1 == "--histogram" ]; then
-  # fun diagnostic
-  grep --no-filename '^tag' _yanis/*.md | \
-    sed -e 's/tags: \[//' -e 's/\]//' -e 's/, /,/g' -e '/^$/ d' | \
-    tr ',' '\n' | \
-    sort | \
-    uniq -c | \
-    awk '{ print $1; }' | \
-    sort -n | \
-    uniq -c | \
-    awk '{ printf("%3s\t%s\n", $2, $1); }'
+DOWRITE=false
+HISTOGRAM=false
+CATEGORIES=false
+TAG_IDENTIFIER='^tags' # regex passed to grep to identify tag lines
+TAG_PREFIX='s/tags: \[//' # first regex passed to sed to strip out of the line
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --write-csv)
+            DOWRITE=true
+            shift;;
+        --histogram)
+            HISTOGRAM=true
+            shift;;
+        --categories)
+            CATEGORIES=true
+            TAG_IDENTIFIER='^category'
+            TAG_PREFIX='s/category: \[//'
+            shift;;
+        *)
+            echo "Unrecognized argument $1"
+            exit 1;;
+    esac
+done
+
+if [[ "$HISTOGRAM" == true ]]; then
+    # fun diagnostic
+    grep --no-filename $TAG_IDENTIFIER _yanis/*.md | \
+      sed -e "$TAG_PREFIX" -e 's/\]//' -e 's/, /,/g' -e '/^$/ d' | \
+      tr ',' '\n' | \
+      sort | \
+      uniq -c | \
+      awk '{ print $1; }' | \
+      sort -n | \
+      uniq -c | \
+      awk '{ printf("%3s\t%s\n", $2, $1); }'
+elif [[ "$DOWRITE" == true ]]; then
+    if [[ "$CATEGORIES" == true ]]; then
+        echo "You cannot write the csv with categories"
+        exit 1
+    fi
+    DATAFILE="_data/tagfreq.csv"
+    echo "count,tag" > $DATAFILE
+    grep --no-filename $TAG_IDENTIFIER _yanis/*.md | \
+      sed -e "$TAG_PREFIX" -e 's/\]//' -e 's/, /,/g' -e '/^$/ d' | \
+      tr ',' '\n' | \
+      sort | \
+      uniq -c | \
+      sed 's/^ *\([0-9]*\) *\(.*\)$/\1,\2/' >> $DATAFILE
 else
-  echo "Unrecognized argument $1."
+    # no option specified, default behavior is to view tags in less
+    grep --no-filename $TAG_IDENTIFIER _yanis/*.md | \
+      sed -e "$TAG_PREFIX" -e 's/\]//' -e 's/, /,/g' -e '/^$/ d' | \
+      tr ',' '\n' | \
+      sort | \
+      uniq -c | \
+      less
 fi
